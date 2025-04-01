8b3ed92680d76c41fe5b15ad6f01c14eff2517c0
Zac Farrell <zachary.farrell@thoughtspot.com>
2025-04-01 14:40:17 -0700

    initial "implementation" using mock data

--- src/parse_tables_extension.cpp
+++ src/parse_tables_extension.cpp
@@ -8,21 +8,69 @@
 #include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
+
 namespace duckdb {
 
-inline void ParseTablesScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "ParseTables "+name.GetString()+" 🐥");
-        });
+struct ParseTablesState : public GlobalTableFunctionState {
+    idx_t row = 0;
+};
+
+struct TableRefResult {
+    string schema;
+    string table;
+    string context;
+};
+
+// BIND function: runs during query planning to decide output schema
+static unique_ptr<FunctionData> Bind(ClientContext &context, 
+                                                TableFunctionBindInput &input, 
+                                                vector<LogicalType> &return_types, 
+                                                vector<string> &names) {
+    return_types = {LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR};
+    // schema name, table name, usage context (from, join, cte, etc)
+    names = {"schema", "table", "context"};
+    return nullptr;
 }
 
+// INIT function: runs before table function execution
+static unique_ptr<GlobalTableFunctionState> MyInit(ClientContext &context,
+    TableFunctionInitInput &input) {
+    return make_uniq<ParseTablesState>();
+}
+
+// EXECUTE function: produces rows
+static void MyFunc(ClientContext &context,
+    TableFunctionInput &data,
+    DataChunk &output) {
+
+    auto &state = (ParseTablesState &)*data.global_state;
+
+    std::cout << "row: " << state.row << std::endl;
+
+    if (state.row >= 1) {
+        return; // no more rows to produce
+    }
+
+    // Example: single string column with 1 row
+    // auto row_count = 1;
+    output.SetCardinality(1);
+
+    output.SetValue(0, 0, Value("my_schema"));
+    output.SetValue(1, 0, Value("my_table"));
+    output.SetValue(2, 0, Value("from"));
+    
+    state.row++;
+}
+
+
+// ---------------------------------------------------
+// EXTENSION SCAFFOLDING
+
 static void LoadInternal(DatabaseInstance &instance) {
-    // Register a scalar function
-    auto parse_tables_scalar_function = ScalarFunction("parse_tables", {LogicalType::VARCHAR}, LogicalType::VARCHAR, ParseTablesScalarFun);
-    ExtensionUtil::RegisterFunction(instance, parse_tables_scalar_function);
+
+    // Register parse_tables
+    TableFunction tf("parse_tables", {LogicalType::VARCHAR}, MyFunc, Bind, MyInit);
+    ExtensionUtil::RegisterFunction(instance, tf);
 }
 
 void ParseTablesExtension::Load(DuckDB &db) {
