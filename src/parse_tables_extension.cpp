37b74b0c430a5330ae4d7f59660928f3e9dd1a6b
Zac Farrell <zachary.farrell@thoughtspot.com>
2025-04-01 14:52:44 -0700

    get access to user input (sql)

--- src/parse_tables_extension.cpp
+++ src/parse_tables_extension.cpp
@@ -15,6 +15,10 @@ struct ParseTablesState : public GlobalTableFunctionState {
     idx_t row = 0;
 };
 
+struct ParseTablesBindData : public TableFunctionData {
+    string sql;
+};
+
 struct TableRefResult {
     string schema;
     string table;
@@ -23,13 +27,25 @@ struct TableRefResult {
 
 // BIND function: runs during query planning to decide output schema
 static unique_ptr<FunctionData> Bind(ClientContext &context, 
-                                                TableFunctionBindInput &input, 
-                                                vector<LogicalType> &return_types, 
-                                                vector<string> &names) {
+                                    TableFunctionBindInput &input, 
+                                    vector<LogicalType> &return_types, 
+                                    vector<string> &names) {
+                                
+    string sql_input = StringValue::Get(input.inputs[0]);
+    std::cout << "sql_input: " << sql_input << std::endl;
+                                                    
+    // always return the same columns:
+
     return_types = {LogicalType::VARCHAR, LogicalType::VARCHAR, LogicalType::VARCHAR};
     // schema name, table name, usage context (from, join, cte, etc)
     names = {"schema", "table", "context"};
-    return nullptr;
+    
+    // create a bind data object to hold the SQL input
+    
+    auto result = make_uniq<ParseTablesBindData>();
+    result->sql = sql_input;
+
+    return std::move(result);
 }
 
 // INIT function: runs before table function execution
@@ -46,6 +62,10 @@ static void MyFunc(ClientContext &context,
     auto &state = (ParseTablesState &)*data.global_state;
 
     std::cout << "row: " << state.row << std::endl;
+
+    auto &bind_data = (ParseTablesBindData &)*data.bind_data;
+
+    std::cout << "Executing for SQL: " << bind_data.sql << std::endl;
 
     if (state.row >= 1) {
         return; // no more rows to produce
